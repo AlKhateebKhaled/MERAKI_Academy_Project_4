4798dfa3c40af1f6a901320e51a2ea2bcaf39f80
AlKhateebKhaled <khaled.hkhateeb@gmail.com>
2024-10-06 22:46:19 +0300

    Implement Product Management APIs

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -36,4 +36,43 @@ const getAllCategories = async (req, res) => {
   }
 };
 
-module.exports = { createCategory, getAllCategories };
+const getCategoryById = async (req, res) => {
+  try {
+    const category = await categoryModel
+      .findById(req.params.id)
+      .populate("subCategories", "parentCategory");
+    console.log(req.params.id);
+    if (!category) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Category not found" });
+    }
+    return res.status(200).json({ success: true, category });
+  } catch (err) {
+    return res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+const deleteCategory = async (req, res) => {
+  try {
+    const category = await categoryModel.findByIdAndDelete(req.params.id);
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: "Category not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Category Deleted Successfully",
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+module.exports = {
+  createCategory,
+  getAllCategories,
+  getCategoryById,
+  deleteCategory,
+};
--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,11 +1,32 @@
 const express = require("express");
-const { createCategory, getAllCategories } = require("../controllers/category");
+const auth = require("../middleware/authentication");
+const authorization = require("../middleware/authorization");
+const accountLockCheck = require("../middleware/accountLockCheck");
+const {
+  createCategory,
+  getAllCategories,
+  getCategoryById,
+  deleteCategory,
+} = require("../controllers/category");
 
 const categoryRouter = express.Router();
 
-categoryRouter.post("/", createCategory);
+categoryRouter.post(
+  "/",
+  auth,
+  authorization("create_category"),
+  createCategory
+);
 
-categoryRouter.get("/", getAllCategories);
+categoryRouter.get("/", accountLockCheck, getAllCategories);
 
+categoryRouter.get("/:id", accountLockCheck, getCategoryById);
+
+categoryRouter.delete(
+  "/:id",
+  auth,
+  authorization("delete_category"),
+  deleteCategory
+);
 
 module.exports = categoryRouter;
