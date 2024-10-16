12640b28348992c5c95203fbaaa7c2192b9fefc9
AlKhateebKhaled <khaled.hkhateeb@gmail.com>
2024-10-16 12:08:56 +0300

    Add NotFound Page with adding style for it

--- frontend/src/Pages/NotFound/index.js
+++ frontend/src/Pages/NotFound/index.js
@@ -1,17 +1,23 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import "./style.css"; 
+
 export default function NotFound() {
   return (
-    <div>
-      <h2>Page not found</h2>
-      <p>
-        nnnnnn nn n n nnnn n n n n n nnnnn n n n nnnn n n nnn n nn n n n n n n n
-        n nnnnnn n n n nn n n n nnnnn{" "}
-      </p>
+    <div className="not-found-container">
+      <div className="not-found-content">
+        <h1 className="not-found-title">404</h1>
+        <h2 className="not-found-subtitle">We can't seem to find the page</h2>
+        <p className="not-found-text">
+          The page you're looking for doesn't exist or has been moved. Don’t
+          worry, we've got plenty of other things for you to check out!
+        </p>
 
-      <p>
-        {" "}
-        Go to the <NavLink to="/">HomePage</NavLink>
-      </p>
+        <NavLink to="/" className="not-found-home-button">
+          <FaHome className="home-icon" /> Back to Home
+        </NavLink>
+      </div>
     </div>
   );
 }
